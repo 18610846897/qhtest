a5e17e77bb6b3bbbd3824bf861fff477cdc40508
18610846897 <546135914@qq.com>
2019-03-20 11:03:39 +0800

    前海http 测试项目

---
+++ RULE/src/main/java/com/qh/asynhttp/Back.java
@@ -0,0 +1,39 @@
+package com.qh.asynhttp;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.util.EntityUtils;
+
+public class Back implements FutureCallback<HttpResponse>
+{
+	private long start = System.currentTimeMillis();
+
+	Back()
+	{
+		System.out.println("响应前还可以干点事情~");
+	}
+
+	public void completed(HttpResponse httpResponse)
+	{
+		try
+		{
+			System.out.println("cost is:" + (System.currentTimeMillis() - start) + ":"
+					+ EntityUtils.toString(httpResponse.getEntity()));
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void failed(Exception e)
+	{
+		System.out.println(" cost is:" + (System.currentTimeMillis() - start) + ":" + e);
+	}
+
+	public void cancelled()
+	{
+		System.out.println("断开http长连接~");
+	}
+}
---
+++ RULE/src/main/java/com/qh/asynhttp/DoHttpRequest.java
@@ -0,0 +1,111 @@
+package com.qh.asynhttp;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+
+@RestController
+public class DoHttpRequest
+{
+	/*
+	 * @Value("${hwq.msg}") private String msg;
+	 */
+
+	@RequestMapping(value = "qh")
+	public void asynHttpDo(HttpServletRequest request, HttpServletResponse response)
+	{
+		System.out.println("-----------------httpPost url------------" + request.getRequestURL());
+		InputStream inputStream = null;
+		try
+		{
+			inputStream = request.getInputStream();
+			InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+			BufferedReader stream = new BufferedReader(reader);
+			String value = "";
+			String message = "";
+			while (null != (value = stream.readLine()))
+			{
+				message += value;
+			}
+			System.out.println("-----------------httpPost body ------------"+JSONObject.toJSONString(message));
+			
+			response.setHeader("Content-Type", "application/json;charset=UTF-8");
+			JSONObject obj = new JSONObject();
+			obj.put("ev", "A");
+			obj.put("sc", "30");
+			obj.put("as", "优选体");
+			response.getWriter().print(obj.toJSONString());
+			
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(inputStream != null)
+				{
+					inputStream.close();
+				}
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@RequestMapping(value = "xxw")
+	public void asynHttp(HttpServletRequest request, HttpServletResponse response)
+	{
+		System.out.println("-----------------httpdo url------------" + request.getRequestURL());
+		InputStream inputStream = null;
+		try
+		{
+			inputStream = request.getInputStream();
+			InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+			BufferedReader stream = new BufferedReader(reader);
+			String value = "";
+			String message = "";
+			while (null != (value = stream.readLine()))
+			{
+				message += value;
+			}
+			System.out.println("-----------------httpdo body ------------"+JSONObject.toJSONString(message));
+			
+			response.setHeader("Content-Type", "application/json;charset=UTF-8");
+			JSONObject obj = new JSONObject();
+			obj.put("ev", "B");
+			obj.put("sc", "40");
+			obj.put("as", "超优体");
+			response.getWriter().print(obj.toJSONString());
+			
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(inputStream != null)
+				{
+					inputStream.close();
+				}
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
